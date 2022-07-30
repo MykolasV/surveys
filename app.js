4bd970d45cc63cf5ea3ad717bc955b721d5ced8e
Mykolas Viningas <m.viningas@gmail.com>
2022-07-30 23:07:23 +0100

    Added flash messages

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const store = require("connect-loki");
+const LokiStore = store(session);
 
 const Survey = require("./lib/survey");
 
@@ -16,6 +20,36 @@ app.set("view engine", "pug");
 app.use(morgan("common"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 31 * 24 * 60 * 60 * 1000, // 31 days in milliseconds
+    path: "/",
+    secure: false,
+  },
+  name: "surveys-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+  // Remember: the secret value is sensitive data that needs protection from prying eyes.
+  // Including it in the source code -- as we do here -- isn't safe.
+  // However, since we only respond to requests from the localhost, we're safe for now.
+  // Don't make this program more widely available without addressing this issue.
+
+  // You should obtain the secret value from an external source that is only available to
+  // your servers, such as a database or a local file.
+  store: new LokiStore({}),
+}));
+
+app.use(flash());
+
+// Extract session info
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
 // Redirect start page
 app.get("/", (req, res) => {
@@ -35,22 +69,27 @@ app.get("/surveys/new", (req, res) => {
 // Create a new survey
 app.post("/surveys", (req, res) => {
   let title = req.body.surveyTitle.trim();
+
   if (title.length === 0) {
+    req.flash("error", "A title was not provided.");
     res.render("new-survey", {
-      errorMessage: "A tittle was not provided.",
+      flash: req.flash(),
     });
   } else if (title.length > 100) {
+    req.flash("error", "Survey title must be between 1 and 100 characters.");
     res.render("new-survey", {
-      errorMessage: "Survey title must be between 1 and 100 characters.",
+      flash: req.flash(),
       surveyTitle: title,
     });
   } else if (surveys.some(survey => survey.title === title)) {
+    req.flash("error", "Survey title must be unique.");
     res.render("new-survey", {
-      errorMessage: "Survey title must be unique.",
+      flash: req.flash(),
       surveyTitle: title,
     });
   } else {
     surveys.push(new Survey(title));
+    req.flash("success", "The survey has been created.");
     res.redirect("/surveys");
   }
 });
