9b1850c93c3452f7ba51df1029f67d87a88ad1f8
Mykolas Viningas <m.viningas@gmail.com>
2022-08-29 23:41:12 +0100

    Added more styles to the survey page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const questions = document.querySelector("#questions");
+
   document.querySelectorAll("form.delete, form.unpublish").forEach(form => {
     form.addEventListener("submit", event => {
       event.preventDefault();
@@ -18,25 +20,69 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  let hiddenFormLink = document.querySelector("#form_link");
-  let formContainer = document.querySelector(".add_question");
-  let formOverlay = document.querySelector("#overlay");
-
-  hiddenFormLink.addEventListener("click", event => {
+  questions.addEventListener("click", event => {
     event.preventDefault();
 
-    formContainer.style.display = "block";
-    formOverlay.style.display = "block";
+    let target = event.target;
+    let li = target.closest("li");
+
+    if (target.closest(".edit_form_link")) {
+      li.querySelector(".edit_question").style.display = "block";
+      li.querySelector(".overlay").style.display = "block";
+    } else if (target.classList.contains("cancel")) {
+      li.querySelector(".edit_question").style.display = "none";
+      li.querySelector(".edit_question").nextElementSibling.style.display = "none";
+    } else if (target.classList.contains("overlay")) {
+      li.querySelector(".edit_question").style.display = "none";
+      target.style.display = "none";
+    }
   });
 
-  document.querySelectorAll("button.cancel, #overlay").forEach(element => {
-    element.addEventListener("click", event => {
-      console.log(event);
-      event.preventDefault();
-      event.stopPropagation();
+  document.querySelector("#add_form_link").addEventListener("click", event => {
+    event.preventDefault();
 
-      formContainer.style.display = "none";
-      formOverlay.style.display = "none";
-    });
+    let targetParent = event.target.parentElement;
+    targetParent.querySelector(".add_question").style.display = "block";
+    targetParent.querySelector(".overlay").style.display = "block";
   });
+
+  document.querySelector("main .overlay").addEventListener("click", event => {
+    let target = event.target;
+
+    document.querySelector(".add_question").style.display = "none";
+    target.style.display = "none";
+  });
+
+  document.querySelector(".add_question button.cancel").addEventListener("click", event => {
+    event.preventDefault();
+
+    document.querySelector(".add_question").style.display = "none";
+    document.querySelector("main .overlay").style.display = "none";
+  });
+
+  
+
+  // let formContainer = document.querySelector(".add_question");
+  // let formOverlay = document.querySelector("#overlay");
+
+  // document.querySelectorAll("#add_form_link, .edit_form_link").forEach(element => {
+  //   element.addEventListener("click", event => {
+  //     event.preventDefault();
+  
+
+  //     formContainer.style.display = "block";
+  //     formOverlay.style.display = "block";
+  //   });
+  // });
+
+  // document.querySelectorAll("button.cancel, .overlay").forEach(element => {
+  //   element.addEventListener("click", event => {
+  //     console.log(event);
+  //     event.preventDefault();
+  //     event.stopPropagation();
+
+  //     formContainer.style.display = "none";
+  //     formOverlay.style.display = "none";
+  //   });
+  // });
 });
